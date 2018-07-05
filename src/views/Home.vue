16838655f186d01d6dbbd8d1e529787a813d70d6
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-07-05 20:41:49 +0200

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,34 @@
 <template>
-  <div>
-    <h1>Spacer</h1>
+  <div class="wrapper">
+    <div class="search">
+      <input id="search" name="search" />
+      <label for="search">Search</label>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'home',
-};
+  name: 'home'};
 </script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 30px;
+  width: 100%;
+}
+.search {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+}
+input {
+  height: 50px;
+  border: 0;
+  border-bottom: 1px solid black;
+}
+</style>
