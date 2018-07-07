1a04de7f4779b84b666b448de8ff2369ecea8a8c
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-07-07 20:10:42 +0200

    axios+lodash

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+import debounce from 'lodash.debounce';
 const API = `https://images-api.nasa.gov`;
 
 export default {
@@ -18,10 +20,13 @@ export default {
     }
   },
   methods: {
-      handleInput(e) {
-          console.log(e.target.value);
-          console.log(this.searchValue);
-      }
+      handleInput: debounce( function() {
+          
+        axios.get(`${API}/search?q=${this.searchValue}&media_type=image`)
+            .then( response => console.log(response) )
+            .catch( error => console.log(error) )
+      
+      }, 500 ),
   }
 };
 </script>
