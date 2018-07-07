948c94f6a05d9cecce4ce32d4738341f04b28767
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-07-07 19:32:48 +0200

    Search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,15 @@
 <template>
-  <div class="wrapper">
-    <div class="search">
-      <input id="search" name="search" />
-      <label for="search">Search</label>
-    </div>
+  <div>
+    <h1>This is HomePage</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'};
+  name: 'Home',
+  };
 </script>
 
 <style lang="scss" scoped>
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0;
-  padding: 30px;
-  width: 100%;
-}
-.search {
-  display: flex;
-  flex-direction: column;
-  width: 300px;
-}
-input {
-  height: 50px;
-  border: 0;
-  border-bottom: 1px solid black;
-}
+
 </style>
---
+++ src/views/Search.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+    <label for="search">Search</label>
+    <input id="search" name="search" v-model="searchValue" @input="handleInput" />      
+    </div>
+  </div>
+</template>
+
+<script>
+const API = `https://images-api.nasa.gov`;
+
+export default {
+  name: 'Search',
+  data() {
+      return {
+          searchValue: '',
+    }
+  },
+  methods: {
+      handleInput(e) {
+          console.log(e.target.value);
+          console.log(this.searchValue);
+      }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 30px;
+  width: 100%;
+}
+.search {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+     
+  label {
+    font-family: Montserrat, sans-serif;
+    text-align: center;
+  }
+}
+input {
+  height: 30px;
+  border: 0;
+  border-bottom: 1px solid black;
+  text-align: center;
+  font-size: 1.1rem; 
+}
+</style>
